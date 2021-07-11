9f097547c25afcce6b888627e372ab1fe03b5028
Jakobi Matagi <jkmatagi@hotmail.com>
2021-07-11 14:53:35 -0600

    added right side of header apps button and profile picture

--- components/Header.js
+++ components/Header.js
@@ -23,8 +23,9 @@ function Header() {
             />
             <h1 className="  ml-2 text-gray-700 text-2xl">Docs</h1>
 
-            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 
-            rounded-lg">
+            {/* search */}
+            <div className="mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 
+            text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
                 <Icon
                     name="search"
                     size="3xl"
@@ -36,6 +37,24 @@ function Header() {
                     className="flex-grow px-5 text-base bg-transparent outline-none selection:bg-white"
                 />
             </div>
+
+            <Button
+                color="gray"
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className="ml-5 md:nl-20 h-20 w-20 border-0"
+            >
+                <Icon name="apps" size="3xl" color="gray" />
+            </Button>
+            <img 
+                loading="lazy"
+                
+                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                src="https://static.hudl.com/users/temp/5009948_a1e993f4defa4ec993ff31114370654d.JPG"
+                alt="profile picture"
+            />
         </div>
     )
 }
